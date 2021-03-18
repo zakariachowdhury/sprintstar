de08e292b3700bec54b1f49831454c05e0de454d
Zakaria Chowdhury <zakaria.cse@gmail.com>
2021-03-18 02:18:24 -0500

    Update poll hosting options

--- sprintstars.py
+++ sprintstars.py
@@ -55,33 +55,47 @@ team_members_dict = {
     'Jane': []
 }
 
-sprint_dict = {
-    TODAY: {
+SPRINT_DEFAULT_VALUES = {
         'name': '',
-        'members': team_members_dict,
+        'members': {},
         'is_poll_open': False,
         'is_poll_closed': False
     }
-}
 
-def save_sprint_config():
-    global sprint_dict
+sprint_config = {}
+
+def set_sprint_default_values(force=False):
+    global sprint_config
+    if TODAY not in sprint_config.keys() or force:
+        sprint_config[TODAY] = SPRINT_DEFAULT_VALUES
+
+def save_sprint_configs_into_file():
+    global sprint_config
     with open(SPRINT_CONFIG_FILENAME, 'w') as f:
-        f.write(str(sprint_dict))
+        f.write(str(sprint_config))
 
-def load_spring_config():
+def load_spring_configs_from_file():
+    global sprint_config
     if not os.path.exists(SPRINT_CONFIG_FILENAME):
-        save_sprint_config()
-        from config import sprint
+        save_sprint_configs_into_file()
+        from config import sprint # to force auto refresh page on file change
 
     try:
         with open(SPRINT_CONFIG_FILENAME) as f:
-            data = f.read()
-            return eval(data)
+            sprint_config = eval(f.read())
     except Exception as e:
         print(e)
         pass
     return None
+
+def get_sprint_config(key, default=None):
+    global sprint_config
+    return sprint_config[TODAY][key] if TODAY in sprint_config.keys() and key in sprint_config[TODAY].keys() else default
+
+def set_sprint_config(key, value):
+    global sprint_config
+    if TODAY in sprint_config.keys() and key in sprint_config[TODAY].keys():
+        sprint_config[TODAY][key] = value
 
 def get_star_members_dict(reverse = False):
     return {k: v for k, v in sorted(team_members_dict.items(), key=lambda item: len(item[1]) if not reverse else -len(item[1])) if len(v)}
@@ -90,29 +104,39 @@ def get_total_participants():
     return sum([len(v) for _, v in team_members_dict.items()])
 
 def display_nomination_form():
-    members_list = ['']
-    feedback = None
-    members_list.extend(list(team_members_dict.keys()))
-    members_list = sorted(members_list)
+    global team_members_dict
+    sprint_name = get_sprint_config('name', '')
+    is_poll_open = get_sprint_config('is_poll_open', False)
+    is_poll_closed = get_sprint_config('is_poll_closed', False)
 
-    nominator = st.selectbox('Your name:', members_list)
-    if nominator:
-        is_not_participating = st.checkbox(LABEL_NOT_PARTICIPATING)
+    if not is_poll_open or len(sprint_name) == 0:
+        st.info('Please wait for the host to open the poll')
+    elif is_poll_closed:
+        st.info('The poll has been closed')
+    else:
+        members_list = ['']
+        feedback = None
+        members_list.extend(list(team_members_dict.keys()))
+        members_list = sorted(members_list)
 
-        if not is_not_participating:
-            is_anonymous = st.checkbox('Nominate anonymously')
-            members_list.remove(nominator)
-            star_member = st.selectbox('Nominate a star for this sprint:', members_list)
+        nominator = st.selectbox('Select your name:', members_list)
+        if nominator:
+            is_not_participating = st.checkbox(LABEL_NOT_PARTICIPATING)
 
-            if len(star_member):
-                feedback = st.text_area('What are the reasons for nominating ' + star_member + '? (optional)')
-                members_list.remove(star_member)
-            
-                if st.checkbox('Submit'):
-                    team_members_dict[star_member].append(Nomination(nominator, feedback, is_anonymous).__dict__)
-                    return True
-        else:
-            return True
+            if not is_not_participating:
+                is_anonymous = st.checkbox('Nominate anonymously')
+                members_list.remove(nominator)
+                star_member = st.selectbox('Nominate a star for this sprint:', members_list)
+
+                if len(star_member):
+                    feedback = st.text_area('What are the reasons for nominating ' + star_member + '? (optional)')
+                    members_list.remove(star_member)
+                
+                    if st.checkbox('Submit'):
+                        team_members_dict[star_member].append(Nomination(nominator, feedback, is_anonymous).__dict__)
+                        return True
+            else:
+                return True
     return False
 
 def display_result(reveal_result = False):
@@ -125,7 +149,7 @@ def display_result(reveal_result = False):
             start_list = ['Star ' + str(len(start_list) - i) for i, _ in enumerate(start_list)]
 
         votes = [len(v) for _, v in star_members_dict.items()]
-        ax.barh(start_list, votes, color='lightcoral')
+        ax.barh(start_list, votes, color='lightcoral' if reveal_result else 'teal')
         ax.xaxis.set_major_locator(MaxNLocator(integer=True))
         ax.set_xlabel('Total votes')
         ax.spines['left'].set_visible(False)
@@ -140,23 +164,25 @@ def display_result(reveal_result = False):
         i = 1
         top_votes = 0
         star_members_dict = get_star_members_dict(True)
-        for name, nominations_list in iter(star_members_dict.items()):
-            if i == 1:
-                top_votes = len(nominations_list)
+        if len(star_members_dict.items()):
+            for name, nominations_list in iter(star_members_dict.items()):
+                if i == 1:
+                    top_votes = len(nominations_list)
 
-            if len(nominations_list):
-                st.markdown(f'### {i}. {name} ({len(nominations_list)} {"votes" if len(nominations_list) > 1 else "vote"}) {":star:" if len(nominations_list) == top_votes else ""}')
-                for nomination in nominations_list:
-                    if nomination['feedback'] is not None and len(nomination['feedback']):
-                        st.markdown(f'- {nomination["feedback"]}' + (f' *-{nomination["nominator"]}*' if not nomination["is_anonymous"] else ''))
-                i += 1
-        st.balloons()
+                if len(nominations_list):
+                    st.markdown(f'**{i}. {name}** *- {len(nominations_list)} {"votes" if len(nominations_list) > 1 else "vote"}* {":star:" if len(nominations_list) == top_votes else ""}')
+                    for nomination in nominations_list:
+                        if nomination['feedback'] is not None and len(nomination['feedback']):
+                            st.markdown(f'- *{nomination["feedback"]}*' + (f' *-{nomination["nominator"]}*' if not nomination["is_anonymous"] else ''))
+                    i += 1
+        else:
+            st.warning('No one participated today')
 
 def display_progress(reveal_result):
     total_participated = get_total_participants()
     total_members = len(team_members_dict)
     if not reveal_result:
-        st.info('The poll is in progress, please wait...')
+        st.info('The poll is in progress...')
         st.progress(total_participated / total_members)
     
         if total_participated == total_members:
@@ -164,40 +190,57 @@ def display_progress(reveal_result):
         else:
             st.markdown(f'*{total_participated} out of {total_members}*')
 
-    return reveal_result
-
 def option_host_poll():
-    global sprint_dict
-    sprint_name = st.text_input('Sprint Name:', sprint_dict[TODAY]['name'])
+    sprint_name = get_sprint_config('name', '')
+
+    is_poll_open = get_sprint_config('is_poll_open', False)
+    is_poll_closed = get_sprint_config('is_poll_closed', False)
+    
+    if not is_poll_closed:
+        sprint_name = st.text_input('Sprint Name:', sprint_name)
+    elif len(sprint_name):
+        st.subheader(sprint_name)
+
     if sprint_name:
-        sprint_dict[TODAY]['name'] = sprint_name
-        open_poll = st.checkbox('Open the poll', sprint_dict[TODAY]['is_poll_open'])
-        sprint_dict[TODAY]['is_poll_open'] = open_poll
-        if open_poll:
-            reveal_result = sprint_dict[TODAY]['is_poll_closed']
+        set_sprint_default_values()
+        set_sprint_config('name', sprint_name)
+        if not is_poll_open:
+            is_poll_open = st.button('Open the poll for nominations', is_poll_open)
+            set_sprint_config('is_poll_open', is_poll_open)
 
-            star_members_dict = get_star_members_dict()
-            if len(star_members_dict.keys()):
-                reveal_result = st.checkbox('Close the poll / Reveal names', get_total_participants() == len(team_members_dict) or reveal_result)
+        if is_poll_open:
+            display_progress(is_poll_closed)
+            display_result(is_poll_closed)
 
-            reveal_result = display_progress(reveal_result)
-            sprint_dict[TODAY]['is_poll_closed'] = reveal_result
+            if not is_poll_closed:
+                if st.button('Close the poll / Reveal names'):
+                    set_sprint_config('is_poll_closed', True)
+                    is_poll_closed = True
+            
+            #star_members_dict = get_star_members_dict()
+            #if len(star_members_dict.keys()):
+                #get_total_participants() == len(team_members_dict)
 
-            display_result(reveal_result)
-        save_sprint_config()
+            if is_poll_closed:
+                st.markdown('---')
+                if st.button(f'Delete the poll'):
+                    set_sprint_default_values(True)
+        save_sprint_configs_into_file()
 
 
 def option_nominate_star():
-    submitted = False
-    reveal_result = False
+    st.subheader(get_sprint_config('name', ''))
     
-    if not reveal_result:
+    submitted = False
+    is_poll_closed = get_sprint_config('is_poll_closed', False)
+    
+    if not is_poll_closed:
         submitted = display_nomination_form()
     
-    if reveal_result or submitted:
-        reveal_result = display_progress(reveal_result)
-        if reveal_result:
-            display_result(reveal_result)
+    if is_poll_closed or submitted:
+        display_progress(is_poll_closed)
+        if is_poll_closed:
+            display_result(is_poll_closed)
 
 def option_settings():
     team_members = ', '.join(list(team_members_dict.keys()))
@@ -205,18 +248,19 @@ def option_settings():
     st.button('Save')
 
 def main():
-    global sprint_dict
-
     st.set_page_config(page_title=PAGE_TITLE, page_icon=APP_ICON)
     st.title(PAGE_TITLE)
+    
+    load_spring_configs_from_file()
 
-    sprint_dict = load_spring_config()
+    # global sprint_config
+    # st.write(sprint_config)
 
     option = st.sidebar.radio('Select an option:', [
         OPTION_NOMINATE_STAR,
         OPTION_HOST_POLL,
         OPTION_SETTINGS
-    ], 1)
+    ])
 
     if option == OPTION_HOST_POLL:
         option_host_poll()
